54104d1e9ea42d861535fbe692094c290935f2cc
黃敏睿 <rayhuang@linkchain.tw>
2022-12-14 15:00:49 +0800

    🧪 add some example

--- test/index.js
+++ test/index.js
@@ -2,9 +2,25 @@ import JSToolkit from '../src/main.js'
 
 const LC = {}
 LC.Toolkit = JSToolkit
-window.LC = window.LC || LC
+window.LC = window.LC || LC;
 
-document.addEventListener("DOMContentLoaded", async () => {
-    await LC.Toolkit.waitForDom("#target")
-    console.log('GOT!')
-});
+document.ready().then(async () => {
+    prepareWait()
+
+    setTimeout(() => {
+        const div = document.createElement('div');
+        div.id = 'target';
+        document.body.appendChild(div);
+    }, 2000)
+})
+
+function prepareWait () {
+    LC.Toolkit.waitForDom("#target").then(() => {
+        console.log('GOT!')
+    })
+}
+
+// document.addEventListener("DOMContentLoaded", async () => {
+//     await LC.Toolkit.waitForDom("#target")
+//     console.log('GOT!')
+// });
